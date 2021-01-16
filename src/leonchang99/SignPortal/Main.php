34dae142941646218c9775c760d88b1d62f3b023
Tefefelix <77537381+Tefefelix@users.noreply.github.com>
2021-01-16 14:58:56 +0100

    Add files via upload

---
+++ src/leonchang99/SignPortal/Main.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace leonchang99\SignPortal;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\math\Vector3;
+use pocketmine\tile\Sign;
+use pocketmine\event\block\SignChangeEvent;
+/** Not currently used but may be later used  */
+use pocketmine\level\Position;
+use pocketmine\entity\Entity;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\item\Item;
+use pocketmine\tile\Tile;
+
+class Main extends PluginBase implements Listener{
+    private $api, $server, $path;
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function playerBlockTouch(PlayerInteractEvent $event){
+        if($event->getBlock()->getID() == 323 || $event->getBlock()->getID() == 63 || $event->getBlock()->getID() == 68){
+            $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+            if(!($sign instanceof Sign)){
+                return;
+            }
+            $sign = $sign->getText();
+            if($sign[0]=='[WORLD]'){
+                if(empty($sign[1]) !== true){
+                    $mapname = $sign[1];
+                    $event->getPlayer()->sendMessage("§6§lFluddelDudel §7> §r§fWelt '".$mapname."' wird vorbereitet");
+                    //Prevents most crashes
+                    $p = $this->api->player->get("username");
+                    $this->api->console->run("cb",$p);
+                }
+            }
+        }
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        //Commands are just for development only, tread carefully...
+        switch($command->getName()){
+            //Very basic world generation command for world teleportation testing
+            case "generate":
+                if(isset($args[0])){
+                    Server::getInstance()->generateLevel($args[0]);
+                    $sender->sendMessage("[SignPortal] World ".$args[0]." is being generated");
+                }else{
+                    $sender->sendMessage("Usage /generate <worldname>");
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    /** Stuff for next update once SignChangeEvent is implemented */
+    public function tileupdate(SignChangeEvent $event){
+        if($event->getBlock()->getID() == 323 || $event->getBlock()->getID() == 63 || $event->getBlock()->getID() == 68){
+            //Server::getInstance()->broadcastMessage("lv1");
+            $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+            if(!($sign instanceof Sign)){
+                return true;
+            }
+            $sign = $event->getLines();
+            if($sign[0]=='[WORLD]'){
+                //Server::getInstance()->broadcastMessage("lv2");
+                if($event->getPlayer()->hasPermission("signportal.create")){
+                    //Server::getInstance()->broadcastMessage("lv3");
+                    if(empty($sign[1]) !==true){
+                        //Server::getInstance()->broadcastMessage("lv4");
+                        if(Server::getInstance()->loadLevel($sign[1])!==false){
+                            //Server::getInstance()->broadcastMessage("lv5");
+                            $event->getPlayer()->sendMessage("§6§lFluddelDudel §7> §r§fPortal zu '".$sign[1]."' erstellt");
+                            return true;
+                        }
+                        $event->getPlayer()->sendMessage("§6§lFluddelDudel §7> §r§fWelt '".$sign[1]."' existiert nicht");
+                        //Server::getInstance()->broadcastMessage("f4");
+                        $event->setLine(0,"[BROKEN]");
+                        return false;
+                    }
+                    $event->getPlayer()->sendMessage("§6§lFluddelDudel §7> §r§fWelt name nicht gestzt");
+                    //Server::getInstance()->broadcastMessage("f3");
+                    $event->setLine(0,"[BROKEN]");
+                    return false;
+                }
+            $event->getPlayer()->sendMessage("§6§lFluddelDudel §7> §r§fDu brauchst die benötigte berechtigung um ein Portal zu erstellen");
+            //Server::getInstance()->broadcastMessage("f2");
+            $event->setLine(0,"[BROKEN]");
+            return false;
+            }
+        }
+        return true;
+    }
+}
